b7ee8813b11dde7075777b235f6dc34155d5a32a
siddiquirizwan@31646@gmail.com <siddiquirizwan31646@gmail.com>
2026-05-17 17:24:00 +0530

    Full project update

--- frontend/src/pages/playlist.jsx
+++ frontend/src/pages/playlist.jsx
@@ -369,7 +369,7 @@ const iconBtn = {
             display: "flex", flexDirection: "column", alignItems: "center", gap: 4,
           }}>
             <img
-              src="https://i.postimg.cc/DZLCn6Sb/Chat-GPT-Image-May-11-2026-02-56-29-PM.png"
+              src="https://i.postimg.cc/wMj8BDkS/Chat-GPT-Image-May-11-2026-02-56-29-PM.png"
               alt="QalbAudio"
               onClick={() => goTo("/")}
               style={{ height: 60, width: "auto", maxWidth: "88%", objectFit: "contain", cursor: "pointer", display: "block" }}
